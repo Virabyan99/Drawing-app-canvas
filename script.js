525c607d5d3f4a29f2510eae7c711cc8e6e8d244
Andranik Virabyan <gmparstone99@gmail.com>
2024-07-10 06:44:41 -0800

    Js changed for mibile phone as well

--- script.js
+++ script.js
@@ -1,86 +1,95 @@
-const canvas = document.getElementById('canvas')
-canvas.width = window.innerWidth - 60
-canvas.height = 400
+const canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth - 60;
+canvas.height = 400;
 
-let context = canvas.getContext('2d')
-let start_background_color = 'white'
-context.fillStyle = start_background_color
-context.fillRect(0, 0, canvas.width, canvas.height)
+let context = canvas.getContext('2d');
+let start_background_color = 'white';
+context.fillStyle = start_background_color;
+context.fillRect(0, 0, canvas.width, canvas.height);
 
-let draw_color = 'black'
-let draw_width = '2'
-let is_drawing = false
+let draw_color = 'black';
+let draw_width = '2';
+let is_drawing = false;
 
-let restore_array = []
-let index = -1
+let restore_array = [];
+let index = -1;
 
 function change_color(element) {
-  draw_color = element.style.background
+  draw_color = element.style.background;
 }
 
-canvas.addEventListener('touchstart', start, false)
-canvas.addEventListener('touchmove', draw, false)
-canvas.addEventListener('mousedown', start, false)
-canvas.addEventListener('mousemove', draw, false)
+canvas.addEventListener('touchstart', start, false);
+canvas.addEventListener('touchmove', draw, false);
+canvas.addEventListener('mousedown', start, false);
+canvas.addEventListener('mousemove', draw, false);
 
-canvas.addEventListener('touchend', stop, false)
-canvas.addEventListener('mouseup', stop, false)
-canvas.addEventListener('mouseout', stop, false)
+canvas.addEventListener('touchend', stop, false);
+canvas.addEventListener('mouseup', stop, false);
+canvas.addEventListener('mouseout', stop, false);
+
+function getEventPosition(event) {
+  if (event.touches && event.touches.length > 0) {
+    return {
+      x: event.touches[0].clientX - canvas.offsetLeft,
+      y: event.touches[0].clientY - canvas.offsetTop
+    };
+  } else {
+    return {
+      x: event.clientX - canvas.offsetLeft,
+      y: event.clientY - canvas.offsetTop
+    };
+  }
+}
 
 function start(event) {
-  is_drawing = true
-  context.beginPath()
-  context.moveTo(
-    event.clientX - canvas.offsetLeft,
-    event.clientY - canvas.offsetTop
-  )
-
-  event.preventDefault()
+  is_drawing = true;
+  context.beginPath();
+  let pos = getEventPosition(event);
+  context.moveTo(pos.x, pos.y);
+  event.preventDefault();
 }
 
 function draw(event) {
   if (is_drawing) {
-    context.lineTo(
-      event.clientX - canvas.offsetLeft,
-      event.clientY - canvas.offsetTop
-    )
-    context.strokeStyle = draw_color
-    context.lineWidth = draw_width
-    context.lineCap = 'round'
-    context.lineJoin = 'round'
-    context.stroke()
+    let pos = getEventPosition(event);
+    context.lineTo(pos.x, pos.y);
+    context.strokeStyle = draw_color;
+    context.lineWidth = draw_width;
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    context.stroke();
   }
-  event.preventDefault()
+  event.preventDefault();
 }
 
 function stop(event) {
   if (is_drawing) {
-    context.stroke()
-    context.closePath()
-    is_drawing = false
+    context.stroke();
+    context.closePath();
+    is_drawing = false;
   }
-  event.preventDefault()
+  event.preventDefault();
   if (event.type != 'mouseout') {
-    restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height))
-    index += 1
+    restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
+    index += 1;
   }
 }
 
 function clear_canvas() {
-  context.fillStyle = start_background_color
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  context.fillRect(0, 0, canvas.width, canvas.height)
+  context.fillStyle = start_background_color;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-  restore_array = []
-  index = -1
+  restore_array = [];
+  index = -1;
 }
 
 function undo_last() {
   if (index <= 0) {
-    clear_canvas()
+    clear_canvas();
   } else {
-    index -= 1
-    restore_array.pop()
-    context.putImageData(restore_array[index], 0, 0)
+    index -= 1;
+    restore_array.pop();
+    context.putImageData(restore_array[index], 0, 0);
   }
 }
